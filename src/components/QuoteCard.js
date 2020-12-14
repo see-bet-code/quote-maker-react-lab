c4d1ec8d66e07fd9a1d5792014958517eb9032d1
see-bet-code <brittani.elan.taylor@gmail.com>
2020-12-14 17:24:40 -0500

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,47 +1,40 @@
 import React from 'react';
-// import { upvoteQuote } from '../actions/quotes'
 
-class QuoteCard extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="card card-inverse card-success card-primary mb-3 text-center">
-          <div className="card-block">
-            <blockquote className="card-blockquote">
-              <p>{this.props.quote.content}</p>
-              <footer>- author <cite title="Source Title">{this.props.quote.author}</cite></footer>
-            </blockquote>
-          </div>
-          <div className="float-right">
-            <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() => this.props.upvoteQuote(this.props.quote.id)}
-              >
-                Upvote
-              </button>
-              <button
-                type="button"
-                className="btn btn-secondary"
-                onClick={() => this.props.downvoteQuote(this.props.quote.id)}
-              >
-                Downvote
-              </button>
-              <button
-                type="button"
-                className="btn btn-danger"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div>Votes: {this.props.quote.votes}</div>
-          </div>
-        </div>
+const QuoteCard = (props) =>
+  <div>
+    <div className="card card-inverse card-success card-primary mb-3 text-center">
+      <div className="card-block">
+        <blockquote className="card-blockquote">
+          <p>{props.quote.content}</p>
+          <footer>- author <cite title="Source Title">{props.quote.author}</cite></footer>
+        </blockquote>
       </div>
-    )
-  }
-}
-  
+      <div className="float-right">
+        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => props.upvoteQuote(props.quote.id)}
+          >
+            Upvote
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => props.downvoteQuote(props.quote.id)}
+          >
+            Downvote
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div>Votes: {props.quote.votes}</div>
+      </div>
+    </div>
+  </div>;
 
 export default QuoteCard;
